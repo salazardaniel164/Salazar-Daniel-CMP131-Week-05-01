b5f544e82dd67f531bf6e3434a554698ad3e7a85
salazardaniel164 <salazargome.danie@student.ccm.edu>
2026-09-24 12:02:02 -0400

    Complete temperature missing even or odd

---
+++ temperature_category.py
@@ -0,0 +1,28 @@
+#Daniel Salazar
+#CMP131-86520
+#Week 05
+# Lab 01
+#Assigment 5 
+#09/24/2026
+print("------------------")
+print(    "Tempature"     )
+print("------------------")
+tempature_degree=int(input("What is the Tempature in Fahrenheit?"))
+print("Tempature: ",tempature_degree, "°F")
+if tempature_degree >= 80:
+    print("Hot")
+elif tempature_degree >=50 and tempature_degree <= 79:
+    print("Warm")
+else:
+    print("Cold")
+#End of Tempature Coding
+
+#EVEN and ODD Code
+print("===============")
+print("Even or Odd")
+print("===============")
+Integer=int(input("Enter any whole number integer"))
+if 
+
+
+
